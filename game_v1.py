fa5f22df5d9cce187aacc35568117651d2f8e4b1
Egorus2 <trukhachev365@gmail.com>
2022-07-09 22:53:53 +0300

    Initial

---
+++ game_v1.py
@@ -0,0 +1,29 @@
+from statistics import mean
+import numpy as np
+
+def random_predict():
+    count = 0
+    number = np.random.randint(1, 101)
+    while True:
+        count += 1
+        if count == 1:
+            predict_num = 50
+            temporary_value = 25
+            if predict_num == number:
+                break
+        elif predict_num < number:
+            predict_num += temporary_value
+            temporary_value = round(temporary_value/2 + 0.01)
+        elif predict_num > number:           
+            predict_num -= temporary_value
+            temporary_value = round(temporary_value/2 + 0.01)
+        else:
+            break
+    return count
+
+lst = []
+for i in range(0, 100):
+    lst.append(random_predict())
+print(mean(lst))
+
+
